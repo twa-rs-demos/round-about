7d4b213626083319f2fabb386748686a71ca251f
XLuoChen <1425145741@qq.com>
2017-03-09 22:10:41 +0800

    add store page

--- themes/spike/category-store.php
+++ themes/spike/category-store.php
@@ -1,7 +1,79 @@
 <?php get_header(); ?>
-<div>
 
-  <h1>hhhhhhhhhh</h1>
+<div id="round-store">
+  <div class="row buy">
+    <div class="col-sm-offset-2 col-sm-8 col-xs-12 buy-image no-padding">
+      <img src="<?php bloginfo('template_url'); ?>/images/buy2.jpg" alt="">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-offset-2 col-xs-offset-1 col-sm-8 col-xs-10 no-padding">
+      <div class="store-taobao"><h2 class="store-taobao-title text-center round-store-title">众爱商店是淘宝的好地方</h2>
+        <p class="content taobao-statement">Roundabout is a social enterprise headed by volunteers.We provide a free
+          service connecting those who wish togive, be that monetaryor otherwise, to those in need.
+          Started in 2008, we were the first charity store inmainland China. We now give support to
+          48 charitable organizations in the Beijing area and many more throughoutChina, Inner Mongolia and
+          Mongolia.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="row shopping-for-children" id="onlinestore">
+    <div class="col-sm-offset-3 col-xs-offset-1 col-sm-6 col-xs-10 no-padding">
+      <div>
+        <h2 class="shopping-title text-center round-store-title"><!-- react-text: 60 -->通过购物来改变孩子的
+          <!-- /react-text --><span>命运</span>
+        </h2>
+        <p class="text-center shopping-statement">所得利润用于支持和帮助困境群体。</p>
+        <div class="col-xs-6 two-dimension-code no-padding">
+          <div class="col-xs-6 inline text-right no-padding relative-positon">
+            <img class="store-picture"
+                 src="<?php bloginfo('template_url'); ?>/images/wechat01.png"
+                 alt=""><span
+              class="store-name">微信商城</span>
+          </div>
+          <div class="col-xs-6 inline text-right no-padding relative-positon">
+            <img class="store-picture"
+                 src="<?php bloginfo('template_url'); ?>/images/taobao01.png"
+                 alt=""><span
+              class="store-name">淘宝商城</span>
+          </div>
+        </div>
+        <div class="col-xs-6 donate-button">
+          <div class="col-xs-11 text-right">
+            <button class="donate-type">捐款</button>
+            <button class="donate-type">捐物</button>
+            <div>... Or if you’re in the giving mood</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row physical-store" id="physicalstore">
+    <div class="col-sm-offset-2 col-xs-offset-1 col-sm-8 col-xs-10 no-padding">
+      <div><h2 class="physical-store-title text-center round-store-title">实体商店</h2>
+        <div class="col-sm-offset-1 col-sm-10 store-info">We have a retail store &amp; warehouse for sorting &amp;
+          storing stock (totalling 2000m2) which is open daily from 9:30am -5:30pm(except Sunday).
+        </div>
+        <div>
+          <div class="col-sm-7 round-about-place">
+            <img src="<?php bloginfo('template_url') ?>/images/donate2.png" alt="">
+          </div>
+          <div class="col-sm-5 round-about no-padding">
+            <div class="round-about-store">
+              <p class="title no-margin">Roundabout</p>
+              <p class="no-margin">Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi
+                District,Beijing. 101302</p>
+              <p class="no-margin">Use Baidu or Gaode Map with keyword #roundabout# will direct you to the Store.</p>
+            </div>
+            <div class="store-position">
+              <img src="<?php bloginfo('template_url') ?>/images/position.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
