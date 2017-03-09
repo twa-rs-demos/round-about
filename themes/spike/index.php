36a90af080770ce147fa8fc15227f224635f10e2
MHwishes <1025383143@qq.com>
2017-03-09 15:02:40 +0800

    add header

--- themes/spike/index.php
+++ themes/spike/index.php
@@ -1,8 +1,4 @@
 <?php get_header(); ?>
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bootsrap.css" type="text/css" media="screen" />
-
-<?php //wp_enqueue_style ('theme-style', get_template_directory_uri().'/bootsrap.css');?>
-
 <div>
     <h1 >body</h1>
 </div>
