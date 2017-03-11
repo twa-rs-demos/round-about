f830bef5c54d48435d96172118820da0bbdd5ca6
MHwishes <1025383143@qq.com>
2017-03-11 09:07:02 +0800

    test

---
+++ themes/spike/category-ourwork.php
@@ -0,0 +1,22 @@
+<?php get_header() ?>
+<?php
+pll_the_languages();
+?>
+
+<?php
+
+$args = array();
+
+
+$pages = get_pages();
+foreach ($pages as $page) {
+  $option = '<option value="' . get_page_link($page->ID) . '">';
+  $option .= $page->post_title;
+  $option .= '</option>';
+  echo $option;
+}
+?>
+
+<?php get_footer() ?>
+
+
