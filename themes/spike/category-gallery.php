bfb52d4ba7c092dbc0d225a90228ca1b767d4647
Miss-qi <1370322806@qq.com>
2017-03-14 15:54:13 +0800

    modify about gallery

---
+++ themes/spike/category-gallery.php
@@ -0,0 +1,33 @@
+<?php get_header() ?>
+
+    <div id="gallery">
+        <div id="gallery-picture">
+            <div class="container"><h1 class="title">ASTOUNDING BOOK FAIR SUCCESS AT THE BRITISH SCHOOL</h1>
+                <div class="text-right carousel-arrowsmall col-xs-offset-1 col-xs-10"><img
+                            src="<?php bloginfo('template_url'); ?>/images/gallery/carousel_arrowsmall_left.png">
+                    4 /
+                    4<img
+                            src="<?php bloginfo('template_url'); ?>/images/gallery/carousel_arrowsmall_right.png">
+                </div>
+            </div>
+            <div class="picture-background">
+                <div class="container">
+                    <div class="col-xs-offset-1 col-xs-10 picture-carousel"><img class="img col-xs-12 no-padding"
+                                                                                 src="<?php bloginfo('template_url'); ?>/images/gallery/gallery_4.jpg">
+                        <div class="carousel-arrowbg-position"><img class="carousel-arrowbg-left"
+                                                                    src="<?php bloginfo('template_url'); ?>/images/gallery/carousel_arrowbg_left.png"><img
+                                    src="<?php bloginfo('template_url'); ?>/images/gallery/carousel_arrowbg_right.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="content" class="container ">
+            <div class="col-xs-offset-1 col-xs-10"><p class="gallery-content">RoundAbout is a social enterprise headed
+                    by volunteers.We provided a free service connecting those who wish to give,be that monetary or
+                    otherwise,to those in need.Started in 2008,we were the first charity store in mainland China.We now
+                    give support to 48 charitable organizations in the Being area and many more throughout China,Inner
+                    Mongolia and Mongolia.</p></div>
+        </div>
+    </div>
+<?php get_footer() ?>
