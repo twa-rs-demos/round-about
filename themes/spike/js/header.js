cad99b50247019c69309f4ea6c17b1df91c39bde
MHwishes <1025383143@qq.com>
2017-03-16 18:22:51 +0800

    page jump in a same page

--- themes/spike/js/header.js
+++ themes/spike/js/header.js
@@ -1,3 +1,29 @@
+var jumpInSamePages = [{
+    page: 'retailstore_zh',
+    jumppositopn: '#physicalstore'
+}, {
+    page: 'onlinestore_zh',
+    jumppositopn: '#onlinestore'
+}, {
+    page: 'retailstore_en',
+    jumppositopn: '#physicalstore'
+}, {
+    page: 'onlinestore_zh',
+    jumppositopn: '#onlinestore'
+}, {
+    page: 'moneytary_donation_zh',
+    jumppositopn: '#donatemoney'
+}, {
+    page: 'itemd_onation-zh',
+    jumppositopn: '#item-donation'
+}, {
+    page: 'moneytary_donation_en',
+    jumppositopn: '#donatemoney'
+}, {
+    page: 'itemd_onation-en',
+    jumppositopn: '#item-donation'
+}];
+
 $(function () {
     let menus = __injectedVars.menus.map((menu) => {
 
@@ -9,6 +35,20 @@ $(function () {
         })
     });
 
+    function getjumpage(jumpInSamePage, submenus, parentLink) {
+        submenus.forEach((submenu) => {
+            if (submenu.slug === jumpInSamePage.page) {
+                submenu.link = `${parentLink}${jumpInSamePage.jumppositopn}`
+            }
+        })
+    }
+
+    jumpInSamePages.forEach((jumpInSamePage) => {
+        menus.forEach((menu) => {
+            getjumpage(jumpInSamePage, menu.subMenus, menu.link);
+        })
+    });
+    
     $("#menuTemplate").tmpl(menus)
         .appendTo("#menu");
 
