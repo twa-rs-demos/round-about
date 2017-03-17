e1a27b2c38c2edca49bae3c7a1f118ea1699b1db
MHwishes <1025383143@qq.com>
2017-03-17 17:48:05 +0800

    fix submenu bug

--- themes/spike/js/header.js
+++ themes/spike/js/header.js
@@ -22,6 +22,18 @@ var jumpInSamePages = [{
 }, {
     page: 'itemd_onation-en',
     jumppositopn: '#item-donation'
+}, {
+    page: 'apply_founding_zh',
+    jumppositopn: 'https://jinshuju.net/f/RhPPwa'
+}, {
+    page: 'apply_founding_en',
+    jumppositopn: 'https://jinshuju.net/f/RhPPwa'
+}, {
+    page: 'donate_link_zh',
+    jumppositopn: ''
+}, {
+    page: 'donate_link_en',
+    jumppositopn: ''
 }];
 
 $(function () {
@@ -35,20 +47,31 @@ $(function () {
         })
     });
 
-    function getjumpage(jumpInSamePage, submenus, parentLink) {
+    function getjumpage(jumpInSamePage, submenus, parentLink, menus) {
         submenus.forEach((submenu) => {
             if (submenu.slug === jumpInSamePage.page) {
-                submenu.link = `${parentLink}${jumpInSamePage.jumppositopn}`
+                submenu.link = `${parentLink}${jumpInSamePage.jumppositopn}`;
+
+                if (jumpInSamePage.page === 'apply_founding_zh' || jumpInSamePage.page === 'apply_founding_en') {
+                    submenu.link = jumpInSamePage.jumppositopn;
+                }
+                if (jumpInSamePage.page === 'donate_link_zh'||jumpInSamePage.page === 'donate_link_en') {
+                    menus.forEach(menu => {
+                        if (menu.slug === 'donate_zh'||menu.slug === 'donate_en') {
+                            submenu.link = menu.link;
+                        }
+                    });
+                }
             }
         })
     }
 
     jumpInSamePages.forEach((jumpInSamePage) => {
         menus.forEach((menu) => {
-            getjumpage(jumpInSamePage, menu.subMenus, menu.link);
+            getjumpage(jumpInSamePage, menu.subMenus, menu.link, menus);
         })
     });
-    
+
     $("#menuTemplate").tmpl(menus)
         .appendTo("#menu");
 
