7f3268a4badf7fd9405803ce16fa72e25014fee5
Miss-qi <1370322806@qq.com>
2017-03-24 17:22:43 +0800

    modify about choice posts by month in newsletter page

--- themes/spike/newslettercenter.php
+++ themes/spike/newslettercenter.php
@@ -15,22 +15,28 @@
         <?php
         $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
         $args = array(
-          'posts_per_page' => 2,
+          'posts_per_page' => 8,
           'category_name' => 'newsletter_center_zh',
           'paged' => $paged
         );
         $posts_query = new WP_Query($args);
         $posts = $posts_query->posts;
-        
+        $time = date('m');
         foreach ($posts as $post) : setup_postdata($post);
-          $img = get_field("img", $post->ID); ?>
-          <div class="col-md-3 col-sm-4 col-xs-6 no-padding story-item"><img
-              src="<?php bloginfo('template_url'); ?>/images/getInvolvedVolunteer/volunteer story01.png"
-              class="story-picture">
-            <p>  <?php echo $post->post_title ?></p>
-            <a href="<?php the_permalink($post->ID); ?>"> Read more > </a>
-          </div>
-        <?php endforeach; ?>
+          $every_time = substr($post->post_date, 5, 2);
+          if ($time != $every_time) {
+            break;
+          } else {
+            $img = get_field("img", $post->ID); ?>
+            <div class="col-md-3 col-sm-4 col-xs-6 no-padding story-item"><img
+                src="<?php bloginfo('template_url'); ?>/images/getInvolvedVolunteer/volunteer story01.png"
+                class="story-picture">
+              <p>  <?php echo $post->post_title ?></p>
+              <a href="<?php the_permalink($post->ID); ?>"> Read more > </a>
+            </div>
+            <?php
+          }
+        endforeach; ?>
       </div>
       <div class="my-pagination pagination-style row">
         <?php page_pagination($posts_query); ?>
@@ -38,4 +44,3 @@
     </div>
   </div>
 </div>
-
