42c817a242c03c7f7e4637c28112f5f88233dd4f
XLuoChen <1425145741@qq.com>
2017-03-27 18:07:49 +0800

    update category-classifieds.php with filter form

--- themes/spike/category-classifieds.php
+++ themes/spike/category-classifieds.php
@@ -23,152 +23,138 @@
       </div>
 
 
-      <div class="static-modal hidden" id="filter">
-        <div tabindex="-1" role="dialog" class="modal" style="display: block;">
-          <div class="modal-dialog">
-            <div class="modal-content" role="document">
-              <div class="modal-header">
-                <div>
-                  <button type="button" class="close" aria-label="Close"><span aria-hidden="true">×</span>
-                  </button>
-                  <h4 class="modal-title">筛选</h4></div>
-              </div>
-              <div class="modal-body">
-                <div class="location"><p>地点</p>
-                  <div>
-                    <button class="filter-button ">北京</button>
-                    <button class="filter-button ">上海</button>
-                    <button class="filter-button ">河北</button>
-                    <button class="filter-button ">河南</button>
-                    <button class="filter-button ">杭州</button>
-                    <button class="filter-button ">成都</button>
-                    <button class="filter-button ">甘肃</button>
-                    <button class="filter-button ">青海</button>
-                    <button class="filter-button ">广西</button>
-                  </div>
-                </div>
-                <div class="organization"><p>机构</p>
-                  <div>
-                    <button class="filter-button ">非营利</button>
-                    <button class="filter-button ">社会企业</button>
-                    <button class="filter-button ">营利</button>
-                  </div>
-                </div>
-                <div class="benefit"><p>受益</p>
-                  <div>
-                    <button class="filter-button ">孩子</button>
-                    <button class="filter-button ">老年人</button>
-                    <button class="filter-button ">白血病</button>
-                    <button class="filter-button ">癌症</button>
-                    <button class="filter-button ">灾难</button>
-                    <button class="filter-button ">HTV/AIDS</button>
-                    <button class="filter-button ">普通的</button>
-                    <button class="filter-button ">教育</button>
-                    <button class="filter-button ">流浪汉</button>
-                    <button class="filter-button ">其他</button>
-                  </div>
-                </div>
-              </div>
+      <div id="filter-form" class="hide" tabindex=1>
+        <div class="form-header">
+          <h4 class="modal-title">筛选</h4>
+        </div>
+        <div class="form-body">
+          <div class="location"><p>地点</p>
+            <span class="filter-button ">北京</span>
+            <span class="filter-button ">上海</span>
+            <span class="filter-button ">河北</span>
+            <span class="filter-button ">河南</span>
+            <span class="filter-button ">杭州</span>
+            <span class="filter-button ">成都</span>
+            <span class="filter-button ">甘肃</span>
+            <span class="filter-button ">青海</span>
+            <span class="filter-button ">广西</span>
+          </div>
+          <div class="organization"><p>机构</p>
+            <span class="filter-button ">非营利</span>
+            <span class="filter-button ">社会企业</span>
+            <span class="filter-button ">营利</span>
+          </div>
+          <div class="benefit"><p>受益</p>
+            <span class="filter-button ">孩子</span>
+            <span class="filter-button ">老年人</span>
+            <span class="filter-button ">白血病</span>
+            <span class="filter-button ">癌症</span>
+            <span class="filter-button ">灾难</span>
+            <span class="filter-button ">HTV/AIDS</span>
+            <span class="filter-button ">普通的</span>
+            <span class="filter-button ">教育</span>
+            <span class="filter-button ">流浪汉</span>
+            <span class="filter-button ">其他</span>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="provide-and-question row">
+        <div class="col-sm-6 col-xs-12 provide">
+          <div class="title">提供</div>
+          <div class="provided-types">
+            <div class="col-xs-6 provided-name">
+              <button>Items</button>
+            </div>
+            <div class="col-xs-6 provided-name">
+              <button>Skills &amp; Expertise</button>
+            </div>
+            <div class="col-xs-6 provided-name">
+              <button>Time</button>
+            </div>
+            <div class="col-xs-6 provided-name">
+              <button>Other</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-xs-12 question">
+          <div class="title">提问</div>
+          <div class="provided-types">
+            <div class="col-xs-6 provided-name">
+              <button>Items</button>
+            </div>
+            <div class="col-xs-6 provided-name">
+              <button>Skills &amp; Expertise</button>
+            </div>
+            <div class="col-xs-6 provided-name">
+              <button>Time</button>
+            </div>
+            <div class="col-xs-6 provided-name">
+              <button>Other</button>
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
-    <div class="provide-and-question row">
-      <div class="col-sm-6 col-xs-12 provide">
-        <div class="title">提供</div>
-        <div class="provided-types">
-          <div class="col-xs-6 provided-name">
-            <button>Items</button>
-          </div>
-          <div class="col-xs-6 provided-name">
-            <button>Skills &amp; Expertise</button>
-          </div>
-          <div class="col-xs-6 provided-name">
-            <button>Time</button>
-          </div>
-          <div class="col-xs-6 provided-name">
-            <button>Other</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-xs-12 question">
-        <div class="title">提问</div>
-        <div class="provided-types">
-          <div class="col-xs-6 provided-name">
-            <button>Items</button>
-          </div>
-          <div class="col-xs-6 provided-name">
-            <button>Skills &amp; Expertise</button>
-          </div>
-          <div class="col-xs-6 provided-name">
-            <button>Time</button>
-          </div>
-          <div class="col-xs-6 provided-name">
-            <button>Other</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="charities-list col-sm-offset-2 col-sm-8 no-padding">
+    <div class="charities-list col-sm-offset-2 col-sm-8 no-padding">
 
 
-    <?php
-    $wp_query = $search->query();
+      <?php
+      $wp_query = $search->query();
 
-    if ($wp_query->have_posts()) :
+      if ($wp_query->have_posts()) :
 
-      $posts = $wp_query->posts;
+        $posts = $wp_query->posts;
 
-      foreach ($posts as $post): ?>
-        <article id="post-<?php get_the_ID(); ?>" <?php post_class(); ?>>
-          <?php $custom_fields = get_post_custom($post->ID); ?>
+        foreach ($posts as $post): ?>
+          <article id="post-<?php get_the_ID(); ?>" <?php post_class(); ?>>
+            <?php $custom_fields = get_post_custom($post->ID); ?>
+            <?php
+            $charityName = $custom_fields['charityName'][0];
+            $charityAddress = $custom_fields['charityAddress'][0];
+            $charityPhone = $custom_fields['charityPhone'][0];
+            $img = get_field("img", $post->ID);
+            $content = $post->post_content;
+            $tags = get_the_tags($post->ID);
+            ?>
+            <div class="row charities">
+              <div class="col-xs-2 no-padding">
+                <img src="<?php echo $img['url']; ?>" alt="charity-img"/>
+              </div>
+              <div class="col-xs-10 no-padding"><h5 class="charity-name"><a
+                    href="<?php the_permalink($post->ID); ?>"><?php echo $charityName; ?></a></h5>
+                <div class="charity-info">
+                  <div><i class="fa fa-home" aria-hidden="true"></i><span
+                      class="span"><?php echo $charityAddress; ?></span>
+                  </div>
+                  <div><i class="fa fa-phone" aria-hidden="true"></i><span
+                      class="span"><?php echo $charityPhone; ?></span>
+                  </div>
+                  <div><p class="span"><?php echo $content; ?></p>
+                  </div>
+                  <div>
+                    <?php foreach ($tags as $tag) : ?>
+                      <span class="tag"><?php echo $tag->name; ?></span>
+                    <?php endforeach; ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
           <?php
-          $charityName = $custom_fields['charityName'][0];
-          $charityAddress = $custom_fields['charityAddress'][0];
-          $charityPhone = $custom_fields['charityPhone'][0];
-          $img = get_field("img", $post->ID);
-          $content = $post->post_content;
-          $tags = get_the_tags($post->ID);
-          ?>
-          <div class="row charities">
-            <div class="col-xs-2 no-padding">
-              <img src="<?php echo $img['url']; ?>" alt="charity-img"/>
-            </div>
-            <div class="col-xs-10 no-padding"><h5 class="charity-name"><a
-                  href="<?php the_permalink($post->ID); ?>"><?php echo $charityName; ?></a></h5>
-              <div class="charity-info">
-                <div><i class="fa fa-home" aria-hidden="true"></i><span
-                    class="span"><?php echo $charityAddress; ?></span>
-                </div>
-                <div><i class="fa fa-phone" aria-hidden="true"></i><span
-                    class="span"><?php echo $charityPhone; ?></span>
-                </div>
-                <div><p class="span"><?php echo $content; ?></p>
-                </div>
-                <div>
-                  <?php foreach ($tags as $tag) : ?>
-                    <span class="tag"><?php echo $tag->name; ?></span>
-                  <?php endforeach; ?>
-                </div>
-              </div>
-            </div>
-          </div>
-        </article>
-        <?php
-      endforeach;
+        endforeach;
 
-    else :
-      echo '<p>Sorry, no results matched your search.</p>';
-    endif;
-    ?>
-    <div class='my-pagination'>
-      <?php page_pagination($wp_query); ?>
+      else :
+        echo '<p>Sorry, no results matched your search.</p>';
+      endif;
+      ?>
+      <div class='my-pagination'>
+        <?php page_pagination($wp_query); ?>
+      </div>
+
+      <?php wp_reset_query(); ?>
     </div>
-
-    <?php wp_reset_query(); ?>
   </div>
-</div>
+
+
