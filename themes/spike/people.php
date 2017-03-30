ef4e9ea5d87d6844bd783d89e41db396171e7745
seafruit <18829290140@163.com>
2017-03-30 14:12:11 +0800

    resolve people pic hover style in IE and layout team part2

--- themes/spike/people.php
+++ themes/spike/people.php
@@ -18,31 +18,31 @@
 <div id="people" class="container-fluid no-padding">
     <div class="picture-max"
          style="background-repeat: no-repeat; background-size: cover; background-image: url(<?php bloginfo('template_url'); ?>/images/people/people.jpg); background-position: center center;"></div>
-    <div id="people-volunteer" class="col-xs-offset-3 col-xs-6"><h2 class="middle-title people-h2-title">以人为本，众人为爱</h2>
-        <p class="middle-describe people-p">加入众爱，一起公益。</p>
+    <div class="row"  id="people-volunteer" >
+        <div class="col-xs-offset-3 col-xs-6"><h2 class="middle-title people-h2-title">以人为本，众人为爱</h2>
+            <p class="middle-describe people-p">加入众爱，一起公益。</p>
 
-        <button type="button" class="button-style become-volunteer center-block" data-toggle="modal" data-target="#volunteers-application-form">
-            加入志愿者行列
-        </button>
-        　
-    </div>
-
-    <div class="modal fade caldera-forms" id="volunteers-application-form" tabindex="-1" role="dialog"
-         aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog form-modal">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                        &times;
-                    </button>
-                    <h4 class="modal-title" id="myModalLabel">
-                        VOLUNTEERS APPLICATION FORM
-                    </h4>
-                </div>
-                <div class="modal-body">
-                    <?php $form = get_post(1234);
-                    echo do_shortcode($form->post_content);
-                    ?>
+            <button type="button" class="button-style become-volunteer center-block" data-toggle="modal" data-target="#volunteers-application-form">
+                加入志愿者行列
+            </button>　
+        </div>
+        <div class="modal fade caldera-forms" id="volunteers-application-form" tabindex="-1" role="dialog"
+             aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog form-modal">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                            &times;
+                        </button>
+                        <h4 class="modal-title" id="myModalLabel">
+                            VOLUNTEERS APPLICATION FORM
+                        </h4>
+                    </div>
+                    <div class="modal-body">
+                        <?php $form = get_post(1234);
+                        echo do_shortcode($form->post_content);
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -89,68 +89,71 @@
 
         </div>
     </div>
-    <div id="ourTeam" class="container">
-        <div class="col-md-offset-1 col-md-10 col-xs-offset-3 col-xs-6"><h3 class="middle-title ourTime-h3">我们的团队</h3>
-        </div>
-        <div class="col-md-offset-1 col-md-9  col-xs-offset-2  col-xs-9">
-            <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+    <div id="ourTeam">
+        <h3 class="middle-title ourTime-h3">我们的团队</h3>
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-8 col-xs-12 col-md-offset-1 col-md-9 ">
+
+                <div class="col-md-offset-2 col-md-2 col-sm-offset-2 col-sm-2 col-xs-6 teamMember"><img class="center-block"
+                                                                                                        src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
+                </div>
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+
+                <div class="col-md-offset-2 col-md-2 col-sm-offset-2 col-sm-2 col-xs-6 teamMember"><img class="center-block"
+                                                                                                        src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
+                </div>
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div>
-                    <div class="col-md-2"></div>
-                    <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
-                                                                   src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"
                                                                src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
                 </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"
-                                                               src='<?php bloginfo('template_url'); ?>/images/people/leaderPicture.png'>
-                </div>
-                <div class="col-md-2 col-xs-5 teamMember"><img class="center-block"></div>
+                <div class="col-sm-2 col-xs-6 teamMember"><img class="center-block"></div>
             </div>
         </div>
-        <div class="col-md-offset-1 col-md-10 col-xs-offset-3 col-xs-6">
-            <button class="button-style become-volunteer  center-block welcome-family" data-toggle="modal" data-target="#volunteers-application-form">加入众爱大家庭</button>
-        </div>
+        <div class="row">
+            <div>
+                <button class="button-style become-volunteer center-block welcome-family"
+                        data-toggle="modal"
+                        data-target="#volunteers-application-form">加入众爱大家庭
+                </button>
+            </div>
 
-        <div class="modal fade caldera-forms" id="volunteers-application-form" tabindex="-1" role="dialog"
-             aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog form-modal">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                            &times;
-                        </button>
-                        <h4 class="modal-title" id="myModalLabel">
-                            VOLUNTEERS APPLICATION FORM
-                        </h4>
-                    </div>
-                    <div class="modal-body">
-                        <?php $form = get_post(1234);
-                        echo do_shortcode($form->post_content);
-                        ?>
+            <div class="modal fade caldera-forms" id="volunteers-application-form" tabindex="-1"
+                 role="dialog"
+                 aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog form-modal">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                                &times;
+                            </button>
+                            <h4 class="modal-title" id="myModalLabel">
+                                VOLUNTEERS APPLICATION FORM
+                            </h4>
+                        </div>
+                        <div class="modal-body">
+                            <?php $form = get_post(1234);
+                            echo do_shortcode($form->post_content);
+                            ?>
+                        </div>
                     </div>
                 </div>
             </div>
